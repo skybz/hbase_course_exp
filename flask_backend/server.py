791103ecd29511c12bb551de87960733cf445384
skybz <sky_bz@outlook.com>
2023-12-06 21:07:03 +0800

    backend update

--- flask_backend/server.py
+++ flask_backend/server.py
@@ -11,12 +11,22 @@ table = connection.table('my_table')  # 将 'your_table_name' 替换为你的 HB
 @app.route('/search')
 def search():
     query_string = request.args.get('query')  # 获取查询参数
+    
+    print(query_string)
+    # 查询 HBase 表中行键包含特定子串的数据
+    results = {}
+    return_result = []
+    for key, data in table.scan():
+        row_key = key.decode()  # 将字节型的行键解码为字符串
+        if query_string in row_key:
+            results[row_key] = data  # 将符合条件的行键和对应数据存储在结果中
 
-    # 使用查询字符串查询 HBase 数据
-    result = table.row(query_string.encode())  # 假设查询字符串作为行键
-
-    # 将查询结果转换为 JSON 格式并返回给前端
-    return jsonify(result)
+    # 输出查询结果
+    for key, data in results.items():
+        return_result.append(f"Row key: {key}, Data: {data}")
+    
+    print(return_result)
+    return return_result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
